4ab0921da5aa838abb38eebebc04ef8f81a6c3a3
erenming <erenming233@gmail.com>
2021-06-30 17:47:10 +0800

    remove conf/edas, same to conf/k8s

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
+# same config of k8s
+if [[ $DICE_CLUSTER_TYPE == 'edas' ]]; then
+    export DICE_CLUSTER_TYPE="kubernetes"
+fi
 
 if [[ $DICE_CLUSTER_TYPE == 'kubernetes' ]]; then
     export CONFIG_DIR=k8s
@@ -7,9 +11,6 @@ else
 fi
 if [ ! $CONFIG_DIR ]; then
     export CONFIG_DIR=dcos
-fi
-if [[ $DICE_CLUSTER_TYPE == 'edas' ]]; then
-    export DICE_CLUSTER_TYPE="kubernetes"
 fi
 
 if [[ $DICE_CLUSTER_TYPE == 'kubernetes' ]]; then
