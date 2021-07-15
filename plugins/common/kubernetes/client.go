c0874006430aef071d920c55eeaad2024b2dfd78
erenming <erenming233@gmail.com>
2021-07-15 10:20:31 +0800

    fix for edge compute

--- plugins/common/kubernetes/client.go
+++ plugins/common/kubernetes/client.go
@@ -2,9 +2,10 @@ package kubernetes
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"log"
 	"os"
+	"time"
 
 	"github.com/influxdata/telegraf/config"
 	"github.com/influxdata/telegraf/plugins/common/tls"
@@ -13,6 +14,7 @@ import (
 )
 
 type Config struct {
+	Mode        string           `toml:"mode"`
 	URL         string           `toml:"url"`
 	Timeout     config.Duration  `toml:"timeout"`
 	BearerToken string           `toml:"bearer_token"`
@@ -20,69 +22,67 @@ type Config struct {
 }
 
 func NewClient(cfg Config) (*k8s.Clientset, error) {
-	var client *k8s.Clientset
-	clusterConfig, err := rest.InClusterConfig()
-	if err != nil {
-		log.Printf("I! unabled to do with InClusterConfig - %v. try with static config...", err)
-		var ca, cert, key, token string
-		if len(cfg.TlsConfig.TLSCA) > 0 {
-			_, err := os.Stat(cfg.TlsConfig.TLSCA)
-			if err == nil {
-				ca = cfg.TlsConfig.TLSCA
-			}
-		}
-		if len(cfg.TlsConfig.TLSCert) > 0 {
-			_, err := os.Stat(cfg.TlsConfig.TLSCert)
-			if err == nil {
-				cert = cfg.TlsConfig.TLSCert
-			}
-		}
-		if len(cfg.TlsConfig.TLSKey) > 0 {
-			_, err := os.Stat(cfg.TlsConfig.TLSKey)
-			if err == nil {
-				key = cfg.TlsConfig.TLSKey
-			}
-		}
-		if len(cfg.BearerToken) > 0 {
-			_, err := os.Stat(cfg.BearerToken)
-			if err == nil {
-				token = cfg.BearerToken
-			}
-		}
-		c, err := k8s.NewForConfig(&rest.Config{
-			TLSClientConfig: rest.TLSClientConfig{
-				Insecure: cfg.TlsConfig.InsecureSkipVerify,
-				CAFile:   ca,
-				CertFile: cert,
-				KeyFile:  key,
-			},
-			Host:          cfg.URL,
-			BearerToken:   token,
-			ContentConfig: rest.ContentConfig{},
-		})
-		if err != nil {
-			return nil, err
-		}
-
-		client = c
-	} else {
-		c, err := k8s.NewForConfig(clusterConfig)
-		if err != nil {
-			return nil, fmt.Errorf("failed to NewForConfig with InClusterConfig. err %s", err)
-		}
-
-		client = c
+	switch cfg.Mode {
+	case "", "in_cluster":
+		log.Printf("I! with InClusterConfig mode")
+		return clientInClusterModel()
+	case "static":
+		log.Printf("I! with static config mode")
+		return clientStaticModel(cfg)
+	default:
+		return nil, errors.New("Invalid mode " + cfg.Mode)
 	}
-	if !HealthCheck(client) {
-		return nil, fmt.Errorf("try to connect apiserver failed")
-	}
-	return client, nil
 }
 
-func HealthCheck(client *k8s.Clientset) bool {
-	_, err := client.Discovery().RESTClient().Get().AbsPath("/healthz").DoRaw(context.TODO())
-	if err != nil {
-		return false
+func clientStaticModel(cfg Config) (*k8s.Clientset, error) {
+	var ca, cert, key, token string
+	if len(cfg.TlsConfig.TLSCA) > 0 {
+		_, err := os.Stat(cfg.TlsConfig.TLSCA)
+		if err == nil {
+			ca = cfg.TlsConfig.TLSCA
+		}
 	}
-	return true
+	if len(cfg.TlsConfig.TLSCert) > 0 {
+		_, err := os.Stat(cfg.TlsConfig.TLSCert)
+		if err == nil {
+			cert = cfg.TlsConfig.TLSCert
+		}
+	}
+	if len(cfg.TlsConfig.TLSKey) > 0 {
+		_, err := os.Stat(cfg.TlsConfig.TLSKey)
+		if err == nil {
+			key = cfg.TlsConfig.TLSKey
+		}
+	}
+	if len(cfg.BearerToken) > 0 {
+		_, err := os.Stat(cfg.BearerToken)
+		if err == nil {
+			token = cfg.BearerToken
+		}
+	}
+	return k8s.NewForConfig(&rest.Config{
+		TLSClientConfig: rest.TLSClientConfig{
+			Insecure: cfg.TlsConfig.InsecureSkipVerify,
+			CAFile:   ca,
+			CertFile: cert,
+			KeyFile:  key,
+		},
+		Host:          cfg.URL,
+		BearerToken:   token,
+		ContentConfig: rest.ContentConfig{},
+	})
+}
+
+func clientInClusterModel() (*k8s.Clientset, error) {
+	clusterConfig, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+	return k8s.NewForConfig(clusterConfig)
+}
+
+func HealthCheck(client *k8s.Clientset, to time.Duration) error {
+	ctx, _ := context.WithTimeout(context.TODO(), to)
+	_, err := client.Discovery().RESTClient().Get().AbsPath("/healthz").DoRaw(ctx)
+	return err
 }
