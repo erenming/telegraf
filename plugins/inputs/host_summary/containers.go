0e76fd21cb003f88d5977c7dbfb6d1c7b43e7122
erenming <erenming233@gmail.com>
2022-03-15 16:19:14 +0800

    remove docker in host_summary

--- plugins/inputs/host_summary/containers.go
+++ plugins/inputs/host_summary/containers.go
@@ -1,16 +1,11 @@
 package hostsummary
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"strconv"
-	"strings"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/filters"
 	"github.com/influxdata/telegraf"
-	"github.com/influxdata/telegraf/plugins/inputs/global/docker"
 	"github.com/influxdata/telegraf/plugins/inputs/global/kubernetes"
 	"github.com/influxdata/telegraf/plugins/inputs/global/node"
 	apiv1 "k8s.io/api/core/v1"
@@ -22,90 +17,91 @@ type ContainersCollector struct{}
 
 // Gather .
 func (c *ContainersCollector) Gather(tags map[string]string, fields map[string]interface{}, acc telegraf.Accumulator) error {
-	info := node.GetInfo()
-	k8s := info.IsK8s()
-	c.getContainers(tags, fields, acc, k8s)
-	if k8s {
-		// k8s 通过 kubelet 统计机器资源使用
-		c.getPodsResource(tags, fields, acc)
+	if !node.GetInfo().IsK8s() {
+		return nil
+	}
+	if err := c.getPodsResource(tags, fields, acc); err != nil {
+		return fmt.Errorf("getPodsResource err: %w", err)
 	}
 	return nil
 }
 
-func (c *ContainersCollector) getContainers(tags map[string]string, fields map[string]interface{}, acc telegraf.Accumulator, k8s bool) error {
-	client, timeout := docker.GetClient()
-	if client == nil {
-		return nil
-	}
-	filterArgs := filters.NewArgs()
-	filterArgs.Add("status", "running")
-	opts := types.ContainerListOptions{
-		Filters: filterArgs,
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-	containers, err := client.ContainerList(ctx, opts)
-	if err != nil {
-		log.Printf("fail to get container list: %s", err)
-		return err
-	}
-	fields["containers"] = len(containers)
-	var (
-		tasks int
-		total containerResource
-	)
-	for _, container := range containers {
-		// 过滤 k8s pause 容器
-		if container.Labels["io.kubernetes.container.name"] == "POD" &&
-			container.Labels["io.kubernetes.docker.type"] == "podsandbox" {
-			continue
-		}
-		// if !strings.HasPrefix(c.Image, "sha256") {
-		// 	if strings.Contains(c.Image, "kubernetes/pause") {
-		// 		continue
-		// 	} else if strings.Contains(c.Image, "dice-third-party/pause") {
-		// 		continue
-		// 	}
-		// }
-		tasks++
-		if !k8s {
-			// dcos 还是按 环境变量方式统计机器 资源
-			res, err := c.getContainerResource(container.ID)
-			if err != nil {
-				log.Printf("fail to inspect container %s: %s", container.ID, err)
-				continue
-			}
-			total.cpuRequest += res.cpuRequest
-			total.cpuLimit += res.cpuLimit
-			total.cpuOrigin += res.cpuOrigin
-			total.memRequest += res.memRequest
-			total.memLimit += res.memLimit
-			total.memOrigin += res.memOrigin
-		}
-	}
-	fields["task_containers"] = tasks
-	if !k8s {
-		fields["cpu_limit_total"] = total.cpuLimit
-		fields["cpu_request_total"] = total.cpuRequest
-		fields["cpu_origin_total"] = total.cpuOrigin
-		fields["mem_limit_total"] = total.memLimit
-		fields["mem_request_total"] = total.memRequest
-		fields["mem_origin_total"] = total.memOrigin
-	}
-	return nil
-}
+// deprecated. Dcos not supported
+// func (c *ContainersCollector) getContainers(tags map[string]string, fields map[string]interface{}, acc telegraf.Accumulator, k8s bool) error {
+// 	client, timeout := docker.GetClient()
+// 	if client == nil {
+// 		return nil
+// 	}
+// 	filterArgs := filters.NewArgs()
+// 	filterArgs.Add("status", "running")
+// 	opts := types.ContainerListOptions{
+// 		Filters: filterArgs,
+// 	}
+// 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+// 	defer cancel()
+// 	containers, err := client.ContainerList(ctx, opts)
+// 	if err != nil {
+// 		log.Printf("fail to get container list: %s", err)
+// 		return err
+// 	}
+// 	fields["containers"] = len(containers)
+// 	var (
+// 		tasks int
+// 		total containerResource
+// 	)
+// 	for _, container := range containers {
+// 		// 过滤 k8s pause 容器
+// 		if container.Labels["io.kubernetes.container.name"] == "POD" &&
+// 			container.Labels["io.kubernetes.docker.type"] == "podsandbox" {
+// 			continue
+// 		}
+// 		// if !strings.HasPrefix(c.Image, "sha256") {
+// 		// 	if strings.Contains(c.Image, "kubernetes/pause") {
+// 		// 		continue
+// 		// 	} else if strings.Contains(c.Image, "dice-third-party/pause") {
+// 		// 		continue
+// 		// 	}
+// 		// }
+// 		tasks++
+// 		if !k8s {
+// 			// dcos 还是按 环境变量方式统计机器 资源
+// 			res, err := c.getContainerResource(container.ID)
+// 			if err != nil {
+// 				log.Printf("fail to inspect container %s: %s", container.ID, err)
+// 				continue
+// 			}
+// 			total.cpuRequest += res.cpuRequest
+// 			total.cpuLimit += res.cpuLimit
+// 			total.cpuOrigin += res.cpuOrigin
+// 			total.memRequest += res.memRequest
+// 			total.memLimit += res.memLimit
+// 			total.memOrigin += res.memOrigin
+// 		}
+// 	}
+// 	fields["task_containers"] = tasks
+// 	if !k8s {
+// 		fields["cpu_limit_total"] = total.cpuLimit
+// 		fields["cpu_request_total"] = total.cpuRequest
+// 		fields["cpu_origin_total"] = total.cpuOrigin
+// 		fields["mem_limit_total"] = total.memLimit
+// 		fields["mem_request_total"] = total.memRequest
+// 		fields["mem_origin_total"] = total.memOrigin
+// 	}
+// 	return nil
+// }
 
 // refer https://github.com/kubernetes-client/python/issues/651
 // accumulate: /pods?fieldSelector=spec.nodeName=node-xxx,status.phase!=Failed,status.phase!=Succeeded
 func (c *ContainersCollector) getPodsResource(tags map[string]string, fields map[string]interface{}, acc telegraf.Accumulator) error {
 	pods, ok := kubernetes.GetPodMap()
 	if !ok {
-		log.Printf("fail to get pods")
+		log.Printf("E! fail to GetPodMap")
 		return nil
 	}
 	var (
-		memReq, memLimit int64
-		cpuReq, cpuLimit float64
+		memReq, memLimit  int64
+		cpuReq, cpuLimit  float64
+		containers, tasks int
 	)
 
 	pods.Range(func(key, value interface{}) bool {
@@ -116,6 +112,14 @@ func (c *ContainersCollector) getPodsResource(tags map[string]string, fields map
 
 		if pod.Status.Phase == apiv1.PodFailed || pod.Status.Phase == apiv1.PodSucceeded {
 			return true
+		}
+
+		containers++ // count pause container
+		for _, c := range pod.Status.ContainerStatuses {
+			if c.State.Running != nil {
+				containers++
+				tasks++
+			}
 		}
 
 		for i := 0; i < len(pod.Spec.Containers); i++ {
@@ -136,6 +140,8 @@ func (c *ContainersCollector) getPodsResource(tags map[string]string, fields map
 	fields["mem_limit_total"] = memLimit
 	fields["mem_request_total"] = memReq
 	fields["mem_origin_total"] = memReq
+	fields["containers"] = containers
+	fields["task_containers"] = tasks
 	return nil
 }
 
@@ -164,57 +170,57 @@ type containerResource struct {
 	memRequest, memLimit, memOrigin float64
 }
 
-func (c *ContainersCollector) getContainerResource(id string) (*containerResource, error) {
-	client, timeout := docker.GetClient()
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-	inspect, err := client.ContainerInspect(ctx, id)
-	if err != nil {
-		return nil, err
-	}
-	var res containerResource
-	for _, line := range inspect.Config.Env {
-		idx := strings.Index(line, "=")
-		if idx < 0 {
-			continue
-		}
-		key := line[0:idx]
-		val := line[idx+1:]
-		switch key {
-		case "DICE_CPU_REQUEST":
-			v, err := strconv.ParseFloat(val, 64)
-			if err == nil {
-				res.cpuRequest = v
-			}
-		case "DICE_CPU_LIMIT":
-			v, err := strconv.ParseFloat(val, 64)
-			if err == nil {
-				res.cpuLimit = v
-			}
-		case "DICE_CPU_ORIGIN":
-			v, err := strconv.ParseFloat(val, 64)
-			if err == nil {
-				res.cpuOrigin = v
-			}
-		case "DICE_MEM_REQUEST":
-			v, err := strconv.ParseFloat(val, 64)
-			if err == nil {
-				res.memRequest = v * 1024 * 1024
-			}
-		case "DICE_MEM_LIMIT":
-			v, err := strconv.ParseFloat(val, 64)
-			if err == nil {
-				res.memLimit = v * 1024 * 1024
-			}
-		case "DICE_MEM_ORIGIN":
-			v, err := strconv.ParseFloat(val, 64)
-			if err == nil {
-				res.memOrigin = v * 1024 * 1024
-			}
-		}
-	}
-	return &res, nil
-}
+// func (c *ContainersCollector) getContainerResource(id string) (*containerResource, error) {
+// 	client, timeout := docker.GetClient()
+// 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+// 	defer cancel()
+// 	inspect, err := client.ContainerInspect(ctx, id)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	var res containerResource
+// 	for _, line := range inspect.Config.Env {
+// 		idx := strings.Index(line, "=")
+// 		if idx < 0 {
+// 			continue
+// 		}
+// 		key := line[0:idx]
+// 		val := line[idx+1:]
+// 		switch key {
+// 		case "DICE_CPU_REQUEST":
+// 			v, err := strconv.ParseFloat(val, 64)
+// 			if err == nil {
+// 				res.cpuRequest = v
+// 			}
+// 		case "DICE_CPU_LIMIT":
+// 			v, err := strconv.ParseFloat(val, 64)
+// 			if err == nil {
+// 				res.cpuLimit = v
+// 			}
+// 		case "DICE_CPU_ORIGIN":
+// 			v, err := strconv.ParseFloat(val, 64)
+// 			if err == nil {
+// 				res.cpuOrigin = v
+// 			}
+// 		case "DICE_MEM_REQUEST":
+// 			v, err := strconv.ParseFloat(val, 64)
+// 			if err == nil {
+// 				res.memRequest = v * 1024 * 1024
+// 			}
+// 		case "DICE_MEM_LIMIT":
+// 			v, err := strconv.ParseFloat(val, 64)
+// 			if err == nil {
+// 				res.memLimit = v * 1024 * 1024
+// 			}
+// 		case "DICE_MEM_ORIGIN":
+// 			v, err := strconv.ParseFloat(val, 64)
+// 			if err == nil {
+// 				res.memOrigin = v * 1024 * 1024
+// 			}
+// 		}
+// 	}
+// 	return &res, nil
+// }
 
 func init() {
 	RegisterCollector("containers", func(map[string]interface{}) Collector {
